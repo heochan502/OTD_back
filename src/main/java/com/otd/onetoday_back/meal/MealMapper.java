3a7bebe81c82ebe45f1b504bd4eb7ac04c668456
greenhc502 <greenhc502@gmail.com>
2025-07-17 17:11:31 +0900

    엑셀파일 인풋 부분 초기단계 1

---
+++ src/main/java/com/otd/onetoday_back/meal/MealMapper.java
@@ -0,0 +1,12 @@
+package com.otd.onetoday_back.meal;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Controller;
+
+
+@Mapper
+
+public interface MealMapper {
+}
---
+++ src/main/java/com/otd/onetoday_back/meal/util/ExcelService.java
@@ -0,0 +1,68 @@
+package com.otd.onetoday_back.meal.util;
+
+
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.ui.Model;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+
+public class ExcelService {
+
+
+    public String excelupload(MultipartFile file, Model model) {
+        log.info("파일명: {}", file.getOriginalFilename() );
+        log.info("파일 크기 {}", file.getSize());
+        try{
+            if (file.isEmpty()) {
+                log.info("파일을 선택하여 주세요");
+            return "/InputFoodDB";
+            }
+            //확장자 검증
+            String fileName = file.getOriginalFilename();
+            if(!fileName.endsWith(".xlsx") &&  !fileName.endsWith(".xls")) {
+                model.addAttribute("message", "excel파일만 업로드 가능합니다");
+                return "/InputFoodDB";
+            }
+            // 객체 생성
+            List<FoodDBExcelDto> foodDBData = new ArrayList<FoodDBExcelDto>();
+
+            //excel파일 처리
+            //스프링 부트에서 Workbook은 Apache POI 라이브러리를 사용하여 엑셀 파일을 다루기 위한 인터페이스
+            //또는 클래스를 의미합니다.
+            //주로 엑셀 파일의 읽기, 쓰기, 조작 등의 작업을 수행할 때 사용됩니다
+            Workbook workbook = WorkbookFactory.create(file.getInputStream());
+            Sheet sheet = workbook.getSheetAt(0);
+
+            //헤더 행 읽기
+
+            Row headerRow = sheet.getRow(0);
+            for (Cell cell : headerRow) {
+                log.debug("{}\\t ", cell.getStringCellValue());
+            }
+
+            // 테이터행 읽기
+            for (int i =1; i<=headerRow.getLastCellNum(); i++ ) {
+                Row row = sheet.getRow(i);
+                // 행 들을 리스트 객페로 구성
+               foodDBData.add(FoodDBExcelDto.builder()
+                               .foodName(row.getCell(0).getStringCellValue())
+                               .foodCategory(row.getCell(1).getStringCellValue())
+                       .build());
+
+            }
+
+
+        }
+        catch (Exception e){}
+
+        return " ";
+    }
+
+}
