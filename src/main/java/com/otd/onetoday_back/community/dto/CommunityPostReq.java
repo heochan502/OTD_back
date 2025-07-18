87823fe3b2bc947e35ddde416190f1c60b9a0ecc
inha915 <dlsgk0915@gmail.com>
2025-07-18 16:04:49 +0900

    수빈언니꺼보려고 잠시 푸시

---
+++ src/main/java/com/otd/onetoday_back/community/dto/CommunityPostReq.java
@@ -0,0 +1,14 @@
+package com.otd.onetoday_back.community.dto;
+
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class CommunityPostReq {
+    //회원정보 예를 들어 박인하를 이름에 넣엇으면 박인하가 들어오는 파일 req
+    private int memberNoLogin;
+    private String title;
+    private String content;
+    private MultipartFile file;  // 클라이언트가 업로드하는 파일
+}
