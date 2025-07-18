eb9ceb861e4b29552579737233d70f08b4e97099
iddadda <iddadda241@gmail.com>
2025-07-18 11:05:25 +0900

    [feat]: 운동기록 상세 조회 통신 추가

--- src/main/java/com/otd/onetoday_back/health/ExerciseLogMapper.java
+++ src/main/java/com/otd/onetoday_back/health/ExerciseLogMapper.java
@@ -1,13 +1,11 @@
 package com.otd.onetoday_back.health;
 
-import com.otd.onetoday_back.health.model.GetExerciseLogDetailReq;
 import com.otd.onetoday_back.health.model.GetExerciseLogDetailRes;
-import com.otd.onetoday_back.health.model.PostExerciseLogDto;
 import com.otd.onetoday_back.health.model.PostExerciseLogReq;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ExerciseLogMapper {
     int saveExerciseLog(PostExerciseLogReq req);
-    GetExerciseLogDetailRes getExerciseLogDetail(GetExerciseLogDetailReq req);
+    GetExerciseLogDetailRes findByExerciseId(int exerciseLogId);
 }
