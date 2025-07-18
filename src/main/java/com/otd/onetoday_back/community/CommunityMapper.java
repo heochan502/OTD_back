b208298ee81fa8edd8b05a1a22b5338f6d1f260c
inha915 <dlsgk0915@gmail.com>
2025-07-18 16:04:49 +0900

    수빈언니꺼보려고 잠시 푸시

---
+++ src/main/java/com/otd/onetoday_back/community/CommunityMapper.java
@@ -0,0 +1,12 @@
+package com.otd.onetoday_back.community;
+
+
+import com.otd.onetoday_back.community.CommunityDto;
+import com.otd.onetoday_back.community.PostDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface CommunityMapper {
+
+}
