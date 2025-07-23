fc473533824899440ee61185a33c1f9577582da5
d-kjh <dkjh9942@gmail.com>
2025-07-23 17:04:18 +0900

    0723초단기예보 -> 초단기실황으로 변경 및 단기예보 추가

--- src/main/java/com/otd/onetoday_back/weather/model/json/Item.java
+++ src/main/java/com/otd/onetoday_back/weather/model/json/Item.java
@@ -11,9 +11,12 @@ public class Item {
     private String baseDate;
     private String baseTime;
     private String category;
+    private int nx;
+    private int ny;
+    // 단기예보
     private String fcstDate;
     private String fcstTime;
     private String fcstValue;
-    private int nx;
-    private int ny;
+    // 초단기실황
+    private String obsrValue;
 }
