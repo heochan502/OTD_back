8b46a45e36fad422aaa1c25229a422a2760b0028
inha915 <dlsgk0915@gmail.com>
2025-08-11 17:30:28 +0900

    댓글 수정 시점

--- src/main/java/com/otd/onetoday_back/community/mapper/MentMapper.java
+++ src/main/java/com/otd/onetoday_back/community/mapper/MentMapper.java
@@ -1,18 +1,11 @@
 package com.otd.onetoday_back.community.mapper;
 
-import com.otd.onetoday_back.community.domain.Ment;
-import com.otd.onetoday_back.community.dto.MentReq;
 import com.otd.onetoday_back.community.dto.MentRes;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface MentMapper {
-    void save(MentReq req);
-
-    List<MentRes> findByPostId(@Param("postId") int postId);
-
-    void deleteById(@Param("commentId") int commentId);
+    List<MentRes> findByPostId(int postId);
 }
